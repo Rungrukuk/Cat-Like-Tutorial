862edd5c541abf837780dbe5c6ce45558d1fa790
Rungrukuk <Rungrukuk@gmail.com>
2023-04-16 15:09:43 +0400

    Shader Compiler Error
    Shader error in 'Graph/Point Surface': Unexpected identifier "SurfaceOutputStandard". Expected one of: sampler sampler1D sampler2D sampler3D samplerCUBE sampler_state SamplerState SamplerComparisonState bool int uint half float double or a user-defined type at line 18

--- Building-A-Graph-Tutorial/Assets/_Script/Graph.cs
+++ Building-A-Graph-Tutorial/Assets/_Script/Graph.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using Color = System.Drawing.Color;
 
-public class Graph : MonoBehaviour
+namespace _Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Graph : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform pointPrefab;
+        [SerializeField,Range(10,100)] private int resolution = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            float step = resolution / 2;
+            Vector3 scale = Vector3.one / step;
+            Vector3 position = Vector3.zero;
+            
+            for (int i = 0; i < resolution; i++)
+            {
+                Transform point = Instantiate(pointPrefab,transform,false);
+                position.x = ((i + 0.5f)/step) - 1f;
+                position.y = position.x * position.x;
+                // point.GetComponent<MeshRenderer>().material;
+                point.localPosition = position;
+                point.localScale = scale;
+
+            }
+        }
     }
 }

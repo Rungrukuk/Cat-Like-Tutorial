4653be6101fa829b6d15217085a086bd60169c9a
Rungrukuk <Rungrukuk@gmail.com>
2023-04-18 17:08:27 +0400

    Mathematical Surfaces - Section 3 has ended

--- Basic/Mathematical Surfaces/Assets/_Script/FunctionLibrary.cs
+++ Basic/Mathematical Surfaces/Assets/_Script/FunctionLibrary.cs
@@ -1,12 +1,39 @@
-
+using static UnityEngine.Mathf;
 
 namespace _Script
 {
     public static class FunctionLibrary
     {
-        public static void Wave()
+        public delegate float Function(float x,float z, float t);
+        
+        public enum FunctionName { Wave,MultiWave,Ripple }
+
+        private static readonly Function[] Functions = {Wave,MultiWave,Ripple};
+
+        public static Function GetFunction(FunctionName name)
+        {
+            return Functions[(int)name];
+        }
+        public static float Wave(float x, float z,float time)
         {
             
+            return Sin(PI * (x  + z + time));
         }
+
+        public static float MultiWave(float x,float z,float time)
+        {
+            float y = Sin(PI * (x + 0.5f*time));
+            y += 0.5f * Sin(2f * PI * (z + time));
+            y += Sin(PI * (x + z + 0.25f * time));
+            return y*(1f/2.5f);
+        }
+
+        public static float Ripple(float x,float z, float time)
+        {
+            float d = Sqrt(x*x + z*z);
+            float y = Sin(PI * (4f*d-time));
+            return y/(1f+10f*d);
+        }
+        
     }
 }
--- Basic/Mathematical Surfaces/Assets/_Script/Graph.cs
+++ Basic/Mathematical Surfaces/Assets/_Script/Graph.cs
@@ -7,6 +7,7 @@ namespace _Script
     {
         [SerializeField] private Transform pointPrefab;
         [SerializeField,Range(10,100)] private int resolution = 10;
+        [SerializeField] private FunctionLibrary.FunctionName function;
         private Transform[] points;
 
         private void Awake()
@@ -14,13 +15,20 @@ namespace _Script
             float step = resolution / 2;
             Vector3 scale = Vector3.one / step;
             Vector3 position = Vector3.zero;
-            points = new Transform[resolution];
+            points = new Transform[resolution * resolution];
 
             
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0,x=0 ,z=0; i < points.Length; i++,x++)
             {
+                if (x == resolution)
+                {
+                    x = 0;
+                    z += 1;
+                }
+
                 Transform point = points[i] = Instantiate(pointPrefab,transform,false);
-                position.x = ((i + 0.5f)/step) - 1f;
+                position.x = ((x + 0.5f)/step) - 1f;
+                position.z = ((z + 0.5f)/step) - 1f;
                 // position.y = position.x * position.x * position.x;
                 point.localPosition = position;
                 point.localScale = scale;
@@ -31,11 +39,12 @@ namespace _Script
         private void Update()
         {
             float time = Time.time;
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
             for (int i = 0; i < points.Length; i++)
             {
                 Transform point = points[i];
                 Vector3 position = point.localPosition;
-                position.y = Mathf.Sin((position.x + time)* Mathf.PI);
+                position.y = f(position.x,position.z, time);
                 point.localPosition = position;
             }
         }

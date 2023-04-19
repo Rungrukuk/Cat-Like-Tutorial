d95c40d346257aa7ce362a750a41db4072f75afd
Rungrukuk <Rungrukuk@gmail.com>
2023-04-19 17:19:16 +0400

    Mathematical Surfaces Has Finished

--- Basic/Mathematical Surfaces/Assets/_Script/FunctionLibrary.cs
+++ Basic/Mathematical Surfaces/Assets/_Script/FunctionLibrary.cs
@@ -1,39 +1,77 @@
+using UnityEngine;
 using static UnityEngine.Mathf;
 
 namespace _Script
 {
     public static class FunctionLibrary
     {
-        public delegate float Function(float x,float z, float t);
+        public delegate Vector3 Function(float u,float v, float time);
         
-        public enum FunctionName { Wave,MultiWave,Ripple }
+        public enum FunctionName { Wave,MultiWave,Ripple,Sphere,Torus }
 
-        private static readonly Function[] Functions = {Wave,MultiWave,Ripple};
+        private static readonly Function[] Functions = {Wave,MultiWave,Ripple,Sphere,Torus};
 
         public static Function GetFunction(FunctionName name)
         {
+
             return Functions[(int)name];
         }
-        public static float Wave(float x, float z,float time)
+        public static Vector3 Wave(float u, float v,float time)
         {
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u  + v + time));
+            p.z = v;
+            return p;
+        }
+
+        public static Vector3 MultiWave(float u,float v,float time)
+        {
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u + 0.5f*time));
+            p.y += 0.5f * Sin(2f * PI * (v + time));
+            p.y += Sin(PI * (u + v + 0.25f * time));
+            p.y *= 1f / 2.5f;
+            p.z = v;
+            return p;
+        }
+
+        public static Vector3 Ripple(float u,float v, float time)
+        {
+            float d = Sqrt(u*u + v*v);
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (4f*d-time));
+            p.y /= 1f + 10f * d;
+            p.z = v;
+            return p;
+        }
+
+        public static Vector3 Sphere(float u, float v, float time)
+        {
+            float r = 0.9f + 0.1f * Sin(PI* (6f*u + 4f* v + time));
+            float s = r * Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(PI * 0.5f * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+
+        public static Vector3 Torus(float u, float v, float time)
+        {
+            float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * time));
+            float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * time));
+            float s = r1 + r2 * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r2 * Sin(PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
             
-            return Sin(PI * (x  + z + time));
         }
 
-        public static float MultiWave(float x,float z,float time)
-        {
-            float y = Sin(PI * (x + 0.5f*time));
-            y += 0.5f * Sin(2f * PI * (z + time));
-            y += Sin(PI * (x + z + 0.25f * time));
-            return y*(1f/2.5f);
-        }
 
-        public static float Ripple(float x,float z, float time)
-        {
-            float d = Sqrt(x*x + z*z);
-            float y = Sin(PI * (4f*d-time));
-            return y/(1f+10f*d);
-        }
-        
     }
 }
--- Basic/Mathematical Surfaces/Assets/_Script/Graph.cs
+++ Basic/Mathematical Surfaces/Assets/_Script/Graph.cs
@@ -14,23 +14,16 @@ namespace _Script
         {
             float step = resolution / 2;
             Vector3 scale = Vector3.one / step;
-            Vector3 position = Vector3.zero;
             points = new Transform[resolution * resolution];
 
             
-            for (int i = 0,x=0 ,z=0; i < points.Length; i++,x++)
+            for (int i = 0; i < points.Length; i++)
             {
-                if (x == resolution)
-                {
-                    x = 0;
-                    z += 1;
-                }
-
                 Transform point = points[i] = Instantiate(pointPrefab,transform,false);
-                position.x = ((x + 0.5f)/step) - 1f;
-                position.z = ((z + 0.5f)/step) - 1f;
+                // position.x = ((x + 0.5f)/step) - 1f;
+                // position.z = ((z + 0.5f)/step) - 1f;
                 // position.y = position.x * position.x * position.x;
-                point.localPosition = position;
+                // point.localPosition = position;
                 point.localScale = scale;
 
             }
@@ -39,13 +32,19 @@ namespace _Script
         private void Update()
         {
             float time = Time.time;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
             FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-            for (int i = 0; i < points.Length; i++)
+            for (int i = 0,x = 0, z = 0; i < points.Length; i++,x++)
             {
-                Transform point = points[i];
-                Vector3 position = point.localPosition;
-                position.y = f(position.x,position.z, time);
-                point.localPosition = position;
+                if (x==resolution)
+                {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = f(u, v, time);
             }
         }
     }
